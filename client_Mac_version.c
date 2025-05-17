03f4b7aa825632fabc1a527ed90fd92a3d48fff7
Tousif Dewan <96633212+Tousif2021@users.noreply.github.com>
2025-05-17 13:21:24 +0200

    client_Mac_version.c

--- client_Mac_version.c
+++ client_Mac_version.c
@@ -10,11 +10,11 @@
 // right content to create a well formed WebService request!
 // Remember that each row must end with \r\n according to the HTTP protocol.
 #define ROW1 "POST /AndersCajander HTTP/1.1\r\n"
-#define ROW2 " Host: 130.229.175.91:8080\r\n"
+#define ROW2 " Host: [Change here! Your peer ip address will go here]:8080\r\n"
 #define ROW3 " Content-Type: application/json\r\n"
 #define ROW4 "Content-Length: 25\r\n"
 #define ROW5 "\r\n"
-#define ROW6 "{ \"first_name\": \"Tousif\" }\r\n"
+#define ROW6 "{ \"first_name\": \"Tousif[! change here as well]\" }\r\n"
 
 int main(int argc, char **argv)
 {
@@ -28,7 +28,7 @@ int main(int argc, char **argv)
    
    struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("130.229.175.91"); //change
+    addr.sin_addr.s_addr = inet_addr("[Change here! Your peer ip address will go here]"); //change
     addr.sin_port = htons(8080); //change
 	if (connect(sock, (const struct sockaddr *)&addr, sizeof(addr)) == -1) {
         return 2;
